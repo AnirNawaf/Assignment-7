3e41ea49becf908ef170cea0840c22509a047c99
AnirNawaf <anirbishal08@gmail.com>
2026-04-17 16:36:13 +0600

    creat notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,7 +11,7 @@ export default function App() {
     <>
       <Navbar />
       <Footer />
-      
+         
     </>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h1 className="p-6 text-red-500 text-6xl flex mt-60 justify-center items-center text-center">404 Page Not Found</h1>;
+}
